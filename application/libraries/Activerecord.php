32361d33fe9215f64b7c965ba9500889ecf0c492
Kris Mitchell <kris.r.m@gmail.com>
2013-02-24 23:57:45 -0700

    more activerecord setup

---
+++ application/libraries/Activerecord.php
@@ -0,0 +1,43 @@
+
+<?php
+/**
+* Init for php.activerecord
+*/
+
+// Load php.activerecord
+require_once APPPATH.'/libraries/php-activerecord/ActiveRecord.php';
+
+// Load CodeIgniter's Model class 
+require_once BASEPATH.'/libraries/Model.php';
+
+class Activerecord {
+    
+    function __construct() {
+        // Load database configuration from CodeIgniter
+        include APPPATH.'/config/database.php';
+        // Get connections from database.php
+        $dsn = array();
+        if ($db) {
+            foreach ($db as $name => $db_values) {
+            	// Convert to dsn format
+            	$dsn[$name] = $db[$name]['dbdriver'] .
+            		'://'   . $db[$name]['username'] .
+            		':'     . $db[$name]['password'] .
+            		'@'     . $db[$name]['hostname'] .
+            		'/'     . $db[$name]['database'];
+            }
+        } 
+        
+        // Initialize ActiveRecord
+        ActiveRecord\Config::initialize(function($cfg) use ($dsn, $active_group){
+            $cfg->set_model_directory(APPPATH.'/models');
+            $cfg->set_connections($dsn);
+            $cfg->set_default_connection($active_group);
+        });
+        
+    }
+}
+
+/* End of file Activerecord.php */
+/* Location: ./application/libraries/Activerecord.php */
+?>
