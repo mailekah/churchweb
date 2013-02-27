ef7b780259a3bd5be660ec6a9e29f6c3cbd51fd3
Kris Mitchell <kris.r.m@gmail.com>
2013-02-27 09:35:30 -0700

    schema builder works

--- application/controllers/schemabuild.php
+++ application/controllers/schemabuild.php
@@ -10,21 +10,28 @@ class SchemaBuild extends CI_Controller {
 	public function build() {
 		$this -> load -> database();
 		$this -> load -> dbforge();
-		
-		
+
 		//
 		// $tables = $this -> db -> list_tables();
 		//
 		// foreach ($tables as $table) {
 		// echo $table;
 		// }
-
+		echo "<h2>Rebuilding Schema</h2>";
+		$dbh = Testing_Table::connection() -> connection;
 		$query = $this -> db -> query("SELECT * FROM sqlite_master WHERE type='table'");
-		
+
 		foreach ($query->result() as $row) {
+			if ($row -> name != 'sqlite_sequence'){
+				$sql = "DROP TABLE $row->name";
+				echo "$sql<br>";
+				$dbh -> exec($sql);				
+			}
 		}
-		$this->dbforge->create_table('table_name');
-		$this->dbforge->drop_table('table_name',TRUE);
+		// foreach($dbh->query('SELECT * FROM sqlite_master WHERE type="table"') as $row) {
+		//print_r($row);
+		// }
+
 		$txt = fopen("datamodel.txt", "r");
 
 		if ($txt) {
@@ -38,24 +45,32 @@ class SchemaBuild extends CI_Controller {
 					continue;
 				} else if (preg_match("/^-\*(.*)$/", $line, $m)) {
 					//ID, primary key, autoincrement
-					echo "<em>$m[1]</em>";
-					echo "<br>";
+					// echo "<em>$m[1]</em>";
+					// echo "<br>";
 					$properties .= "'$m[1]' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, ";
 				} else if (preg_match("/^[-~](.*)$/", $line, $m)) {
 					$properties .= "'$m[1]' TEXT, ";
-					echo $m[1];
-					echo "<br>";
+					// echo $m[1];
+					// echo "<br>";
 				} else {
-					echo "<h3>$line</h3>";
+					//echo "<h3>$line</h3>";
 					if ($table_name != "") {
 						$properties = substr($properties, 0, strlen($properties) - 2);
 						$sql = "CREATE TABLE '$table_name' ($properties);";
+						echo "$sql<br>";
+						$dbh -> exec($sql);
 						$properties = "";
-						echo $sql . "<br>";
 					}
 					$table_name = $line;
 				}
 
+			}
+			if ($table_name != "") {
+				$properties = substr($properties, 0, strlen($properties) - 2);
+				$sql = "CREATE TABLE '$table_name' ($properties);";
+				echo "$sql<br>";
+				$dbh -> exec($sql);
+				$properties = "";
 			}
 			if (!feof($txt)) {
 				echo "Error: unexpected fgets() fail\n";
